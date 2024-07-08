d35b4726e9d33bd3729ee4a4a66aed7ec7962b94
Alexandre Junior <alexandre.junior@lab2dev.com>
2024-07-08 20:12:11 +0000

    feat: list all sales order for s4

--- srv/service.cds
+++ srv/service.cds
@@ -1,7 +1,6 @@
-using {API_SALES_ORDER_SRV}  from './external/API_SALES_ORDER_SRV';
+using {API_SALES_ORDER_SRV} from './external/API_SALES_ORDER_SRV';
 
 service Sap {
-
     entity A_SalesOrder as projection on API_SALES_ORDER_SRV.A_SalesOrder;
-
+    function getSalesOrderAmountBySoldToParty() returns String;
 }
--- srv/service.js
+++ srv/service.js
@@ -13,4 +13,28 @@ module.exports = async (service) => {
 
     return data;
   });
+
+  service.on("getSalesOrderAmountBySoldToParty", async (request) => {
+    const data = await salesOrder.transaction(request).send({
+      query: SELECT.from(salesOrder.entities.A_SalesOrder).limit(1000),
+      headers: {
+        apiKey: "dzbnouSLAV0Rhr8RHMvsLRAA1iXAp4uG",
+      },
+    });
+
+    const salesOrderAmountBySoldToParty = data.reduce((acc, curr) => {
+      if (acc[curr.SoldToParty]) {
+        acc[curr.SoldToParty] = Number(
+          Number(acc[curr.SoldToParty]) + curr.TotalNetAmount
+        ).toFixed(2);
+
+        return acc;
+      }
+
+      acc[curr.SoldToParty] = curr.TotalNetAmount.toFixed(2);
+      return acc;
+    }, {});
+
+    return salesOrderAmountBySoldToParty;
+  });
 };
