ae710870835664cb368c1636b553bedaa585769f
Alexandre Junior <alejunior.dev@gmail.com>
2024-07-08 22:57:45 -0300

    feat: add middleware to tranform odatav4 in odatav2

---
+++ srv/server.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const cds = require('@sap/cds')
+const cors = require('cors')
+const proxy = require('@sap/cds-odata-v2-adapter-proxy')
+
+cds.on('bootstrap', app => {
+  app.use(proxy())
+  app.use(cors({
+    origin: '*', // allow all origins for development; restrict in production
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }))
+})
+
+module.exports = cds.server
